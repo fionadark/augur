bd28ccd121410646d4899018d5a6e70e261209a0
fionadark <brigiddark@gmail.com>
2025-09-16 11:08:30 -0700

    reorganize test file setup

--- app/src/test/java/ImgUtilsTest.java
+++ app/src/test/java/ImgUtilsTest.java
@@ -1,9 +1,8 @@
-package augur;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
+import augur.ImgUtils;
 
 public class ImgUtilsTest {
 
--- app/src/test/java/TarotTest.java
+++ app/src/test/java/TarotTest.java
@@ -1,9 +1,8 @@
-package augur;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
+import augur.Tarot;
 
 public class TarotTest {
 
