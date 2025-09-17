047fa68b62814dc6f6de0451ea958a9644940622
fionadark <brigiddark@gmail.com>
2025-09-17 13:30:33 -0700

    clarify variable names

--- app/src/main/java/augur/Tarot.java
+++ app/src/main/java/augur/Tarot.java
@@ -68,38 +68,38 @@ public class Tarot implements Callable<String> {
      * Displays the results of a tarot reading to the command line.
      * The output varies depending on the type of spread chosen (one-card, three-card, or ten-card/celtic cross).
      * 
-     * @param curSpread   the Spread object containing the cards and their meanings for the reading
+     * @param currentSpread   the Spread object containing the cards and their meanings for the reading
      * @param userChoice  a String representing the user's selected spread type (e.g., "1", "3", or "10")
      * @throws IllegalArgumentException if the userChoice does not correspond to a valid spread
      */
-    public void tellFuture(Spread curSpread, String userChoice) {
+    public void tellFuture(Spread currentSpread, String userChoice) {
 
         // Print results of a celtic cross (10-card) spread
         if(userChoice.contains("10")) {
             System.out.println("Ten cards present themselves to you...\n");
-            System.out.println("Your present: " + curSpread.getCards().get(0).getName() + "\nThis card represents: " + curSpread.getCards().get(0).getMeaningUp() + "\n");
-            System.out.println("Your current challenge: " + curSpread.getCards().get(1).getName() + "\nThis card represents: " + curSpread.getCards().get(1).getMeaningUp() + "\n");
-            System.out.println("Your past: " + curSpread.getCards().get(2).getName() + "\nThis card represents: " + curSpread.getCards().get(2).getMeaningUp() + "\n");
-            System.out.println("Your future: " + curSpread.getCards().get(3).getName() + "\nThis card represents: " + curSpread.getCards().get(3).getMeaningUp() + "\n");
-            System.out.println("Your conscious: " + curSpread.getCards().get(4).getName() + "\nThis card represents: " + curSpread.getCards().get(4).getMeaningUp() + "\n");
-            System.out.println("Your subconscious: " + curSpread.getCards().get(5).getName() + "\nThis card represents: " + curSpread.getCards().get(5).getMeaningUp() + "\n");
-            System.out.println("The cards' advice: " + curSpread.getCards().get(6).getName() + "\nThis card represents: " + curSpread.getCards().get(6).getMeaningUp() + "\n");
-            System.out.println("Your external influences: " + curSpread.getCards().get(7).getName() + "\nThis card represents: " + curSpread.getCards().get(7).getMeaningUp() + "\n");
-            System.out.println("Your hopes and fears: " + curSpread.getCards().get(8).getName() + "\nThis card represents: " + curSpread.getCards().get(8).getMeaningUp() + "\n");
-            System.out.println("Your outcome: " + curSpread.getCards().get(9).getName() + "\nThis card represents: " + curSpread.getCards().get(9).getMeaningUp() + "\n");
+            System.out.println("Your present: " + currentSpread.getCards().get(0).getName() + "\nThis card represents: " + currentSpread.getCards().get(0).getMeaningUp() + "\n");
+            System.out.println("Your current challenge: " + currentSpread.getCards().get(1).getName() + "\nThis card represents: " + currentSpread.getCards().get(1).getMeaningUp() + "\n");
+            System.out.println("Your past: " + currentSpread.getCards().get(2).getName() + "\nThis card represents: " + currentSpread.getCards().get(2).getMeaningUp() + "\n");
+            System.out.println("Your future: " + currentSpread.getCards().get(3).getName() + "\nThis card represents: " + currentSpread.getCards().get(3).getMeaningUp() + "\n");
+            System.out.println("Your conscious: " + currentSpread.getCards().get(4).getName() + "\nThis card represents: " + currentSpread.getCards().get(4).getMeaningUp() + "\n");
+            System.out.println("Your subconscious: " + currentSpread.getCards().get(5).getName() + "\nThis card represents: " + currentSpread.getCards().get(5).getMeaningUp() + "\n");
+            System.out.println("The cards' advice: " + currentSpread.getCards().get(6).getName() + "\nThis card represents: " + currentSpread.getCards().get(6).getMeaningUp() + "\n");
+            System.out.println("Your external influences: " + currentSpread.getCards().get(7).getName() + "\nThis card represents: " + currentSpread.getCards().get(7).getMeaningUp() + "\n");
+            System.out.println("Your hopes and fears: " + currentSpread.getCards().get(8).getName() + "\nThis card represents: " + currentSpread.getCards().get(8).getMeaningUp() + "\n");
+            System.out.println("Your outcome: " + currentSpread.getCards().get(9).getName() + "\nThis card represents: " + currentSpread.getCards().get(9).getMeaningUp() + "\n");
         // Print results of a three-card spread
         } else if (userChoice.contains("3")) {
             System.out.println("Three cards present themselves to you...\n");
-            System.out.println("Your past: " + curSpread.getCards().get(0).getName());
-            System.out.println(curSpread.getCards().get(0).getMeaningRev() + "\n");
-            System.out.println("Your present: " + curSpread.getCards().get(1).getName());
-            System.out.println(curSpread.getCards().get(1).getMeaningRev() + "\n");
-            System.out.println("Your future: " + curSpread.getCards().get(2).getName());
-            System.out.println(curSpread.getCards().get(2).getMeaningRev() + "\n");
+            System.out.println("Your past: " + currentSpread.getCards().get(0).getName());
+            System.out.println(currentSpread.getCards().get(0).getMeaningRev() + "\n");
+            System.out.println("Your present: " + currentSpread.getCards().get(1).getName());
+            System.out.println(currentSpread.getCards().get(1).getMeaningRev() + "\n");
+            System.out.println("Your future: " + currentSpread.getCards().get(2).getName());
+            System.out.println(currentSpread.getCards().get(2).getMeaningRev() + "\n");
         // Print results of a one-card spread
         } else if (userChoice.contains("1")) {
-            System.out.println("The card that presents itself to you is " + curSpread.getCards().get(0).getName() + "\n");
-            System.out.println("This card represents: " + curSpread.getCards().get(0).getMeaningRev() + "\n");
+            System.out.println("The card that presents itself to you is " + currentSpread.getCards().get(0).getName() + "\n");
+            System.out.println("This card represents: " + currentSpread.getCards().get(0).getMeaningRev() + "\n");
         // Handle invalid input
         } else {
             System.out.println("No cards present themselves to you. Your future remains murky!\n");
