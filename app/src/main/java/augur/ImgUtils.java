cc9d6ec035e8d3276e3f0494d51b6bb23541560d
fionadark <brigiddark@gmail.com>
2025-09-17 13:01:02 -0700

    update ImgUtils documentation

--- app/src/main/java/augur/ImgUtils.java
+++ app/src/main/java/augur/ImgUtils.java
@@ -7,19 +7,26 @@ import java.util.Base64;
 
 public class ImgUtils {
 
-    // getEncodedImgString() will encode a given image in base 64
-    // returns encoded img as a String
+    /**
+     * Encodes the specified image file as a Base64 string.
+     * The image file is expected to be located at "src/main/resources/images/{imgName}.jpg" relative to the project directory.
+     *
+     * @param imgName the name of the image file (without extension) to encode
+     * @return a Base64-encoded string representing the image contents
+     * @throws IllegalArgumentException if imgName is null or empty
+     * @throws IOException if the image file cannot be read
+     */
     public static String getEncodedImgString(String imgName) {
-        // verify valid imgName
+        // Verify valid imgName
         if(imgName.isEmpty() || imgName == null) {
             throw new IllegalArgumentException("Invalid image name.");
         }
 
-        // path to image should be : userDir/src/main/resources/images/imgName.jpg
+        // Get path to image
         String img = System.getProperty("user.dir") + "/src/main/resources/images/" + imgName + ".jpg";
         String encodedImgString = "";
 
-        // encode image in base 64
+        // Encode image in base 64
         try {
             byte[] imgAsBytes = Files.readAllBytes(Paths.get(img));
             encodedImgString = Base64.getEncoder().encodeToString(imgAsBytes);
@@ -30,8 +37,15 @@ public class ImgUtils {
         return encodedImgString;
     }
 
-    // getImgHTMLTag() creates and returns an HTML tag with the given image string
-    // if the card is reversed, the img tag includes "transform: rotate(160deg)" so image will display upside down
+    /**
+     * Generates an HTML <img> tag using the provided Base64-encoded image string.
+     * If the card is reversed, the image is displayed upside down by applying a 180-degree rotation.
+     *
+     * @param reversed true if the card is reversed and the image should be rotated; false otherwise
+     * @param img the Base64-encoded string representing the image
+     * @return an HTML <td> element containing the image tag with appropriate styling
+     * @throws IllegalArgumentException if the img argument is null
+     */
     public static String getImgHTMLTag(boolean reversed, String img) {
         String tag = "";
 
